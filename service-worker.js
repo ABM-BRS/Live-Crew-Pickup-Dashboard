7d1492f991ec9c91573e3343070b0860d562f3e9
ABM-BRS <chayk@hotmail.co.uk>
2026-03-20 14:03:52 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,14 @@
-self.addEventListener('install', () => {});
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'abm-crew-cache-v1';
+const urlsToCache = ['./', './index.html', './manifest.json', './icon.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
