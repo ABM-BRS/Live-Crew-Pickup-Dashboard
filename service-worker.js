bd430bc34a8ec6326b5f23abd89fb7406bacf4d9
ABM-BRS <chayk@hotmail.co.uk>
2026-03-23 10:10:59 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,18 @@
 const CACHE_NAME = 'abm-crew-cache-v1';
-const urlsToCache = [
-  './',
-  './manifest.json',
-  // add other assets like icons here if needed
-];
+const urlsToCache = ['./','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-    ))
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
+  )));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
-  );
+self.addEventListener('fetch', event=>{
+  event.respondWith(caches.match(event.request).then(resp=>resp || fetch(event.request)));
 });
